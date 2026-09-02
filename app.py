85c0d81589d2be56e005d01f5d2bbc7e86d74bad
halaahmed20124-gif <halaahmed20124@gmail.com>
2026-09-03 00:29:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1018,7 +1018,7 @@ elif page == "📷 Scan":
             with st.container(border=True):
 
                 st.markdown(
-                    f"### { TEXT[language]["Biotechnology & Organic Waste"]
+                    f"### {TEXT[language]['Biotechnology&_Organic Waste']}"
                 )
 
                 if biotech_info["is_organic"]:
