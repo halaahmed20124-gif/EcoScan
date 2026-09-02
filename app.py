b48d431fed5ef239edd4b6c04905c9428bf785cf
halaahmed20124-gif <halaahmed20124@gmail.com>
2026-09-03 01:04:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -962,7 +962,7 @@ elif page == "📷 Scan":
             if confidence >= 60:
 
                 st.success(
-                    f"{ TEXT[language]['best-choice']} — "
+                    f"{ TEXT[language]['best_choice']} — "
                     f"{waste_info['action']}"
                 )
 
